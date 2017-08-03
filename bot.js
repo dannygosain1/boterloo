d581504fc4d68ac7492576b09a081d4fb278131e
dannygosain1 <dgosain@uwaterloo.ca>
2017-08-02 20:35:07 -0400

    setting up to push to messenger

--- bot.js
+++ bot.js
@@ -1,3 +1,36 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Process application/json
+app.use(bodyParser.json())
+
+// Index route
+app.get('/', function (req, res) {
+	res.send('Hello world, I am a chat bot')
+})
+
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+		res.send(req.query['hub.challenge'])
+	}
+	res.send('Error, wrong token')
+})
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+	console.log('');
+	// console.log('running on port', app.get('port'))
+})
 const builder = require('botbuilder'); // provides access to BotBuilder
 const https = require('https'); // use HTTPS module
 const connector = new builder.ConsoleConnector().listen(); // connect to console
